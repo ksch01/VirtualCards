47ed0c3b46d1a093181d5f893c23435f9cd974d0
kev <ks1202210@gmail.com>
2022-05-28 15:15:06 +0200

    created CardStack class

---
+++ app/src/main/java/com/example/virtualcards/model/CardStack.java
@@ -0,0 +1,54 @@
+package com.example.virtualcards.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class CardStack extends Card {
+    private ArrayList<Card> cards;
+
+    CardStack(float x, float y, Card.Suit suit, Card.Value value) {
+        super(x, y, suit, value);
+    }
+
+    protected static CardStack stackCards(Card card, Card to){
+        CardStack stack;
+        if(card instanceof CardStack){
+            stack = (CardStack) card;
+        }else {
+            stack = new CardStack(to.x, to.y, null, null);
+            stack.add(card);
+        }
+        stack.add(to);
+        return stack;
+    }
+
+    private void add(Card card){
+        if(card instanceof CardStack){
+            cards.addAll(((CardStack) card).cards);
+        }else {
+            cards.add(card);
+        }
+
+    }
+
+    @Override
+    public Card.Suit getSuit(){
+        return cards.get(0).suit;
+    }
+
+    @Override
+    public Card.Value getValue(){
+        return cards.get(0).value;
+    }
+
+    @Override
+    public boolean isFaceUp(){
+        return cards.get(0).isFaceUp();
+    }
+
+    @Override
+    protected void flip(){
+        cards.forEach((card) -> {card.flip();});
+        Collections.reverse(cards);
+    }
+}
